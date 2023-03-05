d06e25bebed3dbabe42c2fbeccfea5ba92e700f3
Pascal Diehm <pdiehm8@gmail.com>
2023-03-05 21:09:35 +0100

    Added variables

---
+++ src/variables.ts
@@ -0,0 +1,9 @@
+import { CompanionVariableDefinition } from "@companion-module/base";
+
+export function GetVariables(): CompanionVariableDefinition[] {
+    return [
+        { variableId: "version", name: "VSCode version" },
+        { variableId: "language", name: "Language (current file)" },
+        { variableId: "lines", name: "Line count (current file)" },
+    ];
+}
