9e83c517ce373115b9461d082e9db051606aad31
Cristian Martinez <cristian.martinez@cogniteva.fr>
2023-12-06 20:17:23 +0100

    feat: Enhance interaction with commandsocket
    This commit adds new variables and modifies the extension to provide
    status information when requested via the get-status action in the
    vscode-commandsocket extension.
    
    Additionally, it updates the GetFeedbacks function to generate feedback
    entries for status-related variables automatically.
    
    These changes are motivated by the need to provide additional status
    information from Visual Studio Code to interact with companion supported
    controllers.
    
    See also
    
    - pascaldiehm/vscode-commandsocket/pull/1

--- src/variables.ts
+++ src/variables.ts
@@ -6,5 +6,14 @@ export function GetVariables(): CompanionVariableDefinition[] {
         { variableId: "language", name: "Language (current file)" },
         { variableId: "lines", name: "Line count (current file)" },
         { variableId: "commands", name: "Command count" },
+        { variableId: "status_debug_active_session", name: "Status debug active session" },
+        { variableId: "status_editor_column_number", name: "Status editor column number" },
+        { variableId: "status_editor_document_name", name: "Status editor document name" },
+        { variableId: "status_editor_encoding", name: "Status editor encoding" },
+        { variableId: "status_editor_error_count", name: "Status editor error count" },
+        { variableId: "status_editor_language_id", name: "Status editor LanguageId" },
+        { variableId: "status_editor_line_number", name: "Status editor line number" },
+        { variableId: "status_git_branch", name: "Status git branch" },
+        { variableId: "status_workspace_name", name: "Status workspace name" },
     ];
 }
