9ef18a921ebaa276225d7eae53aef98e5df634b0
Pascal Diehm <pdiehm8@gmail.com>
2024-01-07 21:53:13 +0100

    Rewrite and feature update
    - Use new encryption system for better performance (see pascaldiehm/vscode-commandsocket@e9895ed)
    - New actions
        - Buttons for notifications
        - Input and Select box
        - Manual command reload
        - Specify additional command arguments (Closes #1)
    - New variables for user interaction response (notification buttons, input and pick)
    - Extensive feedback support (range check, contains, matches)

--- src/config.ts
+++ src/config.ts
@@ -1,81 +1,70 @@
-import { Regex, SomeCompanionConfigField } from "@companion-module/base";
+import { Regex, SomeCompanionConfigField } from '@companion-module/base'
 
-export interface Config {
-    host: string;
-    port: number;
-    password: string;
-    reconnect: number;
-    reloadCommands: number;
-    reloadState: number;
+export type Config = {
+	host: string
+	port: number
+	password: string
+	reconnect: number
+	reloadCommands: number
+	reloadState: number
 }
 
-export const DefaultConfig: Config = {
-    host: "127.0.0.1",
-    port: 6783,
-    password: "",
-    reconnect: 5000,
-    reloadCommands: 60000,
-    reloadState: 500,
-};
-
-export function GetConfig(): SomeCompanionConfigField[] {
-    return [
-        {
-            id: "host",
-            type: "textinput",
-            label: "Target IP",
-            width: 6,
-            default: DefaultConfig.host,
-            required: true,
-            regex: Regex.IP,
-        },
-        {
-            id: "port",
-            type: "number",
-            label: "Target port",
-            width: 6,
-            default: DefaultConfig.port,
-            required: true,
-            min: 0,
-            max: 99999,
-        },
-        {
-            id: "password",
-            type: "textinput",
-            label: "Password",
-            width: 6,
-            default: DefaultConfig.password,
-            required: false,
-        },
-        {
-            id: "reconnect",
-            type: "number",
-            label: "Reconnect delay (0 to disable)",
-            width: 6,
-            default: DefaultConfig.reconnect,
-            required: true,
-            min: 0,
-            max: 60000,
-        },
-        {
-            id: "reloadCommands",
-            type: "number",
-            label: "Fetch delay (commands)",
-            width: 6,
-            default: DefaultConfig.reloadCommands,
-            required: true,
-            min: 0,
-            max: 3600000,
-        },
-        {
-            id: "reloadState",
-            type: "number",
-            label: "Fetch delay (state)",
-            width: 6,
-            default: DefaultConfig.reloadState,
-            required: true,
-            min: 0,
-            max: 60000,
-        },
-    ];
-}
+export const CONFIG: SomeCompanionConfigField[] = [
+	{
+		id: 'host',
+		type: 'textinput',
+		label: 'Target IP',
+		width: 6,
+		default: '127.0.0.1',
+		required: true,
+		regex: Regex.IP,
+	},
+	{
+		id: 'port',
+		type: 'number',
+		label: 'Target Port',
+		width: 6,
+		default: 6783,
+		required: true,
+		min: 0,
+		max: 65535,
+	},
+	{
+		id: 'password',
+		type: 'textinput',
+		label: 'Password',
+		width: 6,
+		default: '',
+		required: false,
+	},
+	{
+		id: 'reconnect',
+		type: 'number',
+		label: 'Reconnect delay (ms, 0 to disable)',
+		width: 6,
+		default: 5000,
+		required: true,
+		min: 0,
+		max: 60000,
+	},
+	{
+		id: 'reloadCommands',
+		type: 'number',
+		label: 'Command fetch delay (ms)',
+		width: 6,
+		default: 60000,
+		required: true,
+		min: 100,
+		max: 3600000,
+	},
+	{
+		id: 'reloadState',
+		type: 'number',
+		label: 'State fetch delay (ms)',
+		width: 6,
+		default: 500,
+		required: true,
+		min: 100,
+		max: 60000,
+	},
+]
--- src/variables.ts
+++ src/variables.ts
@@ -1,19 +1,29 @@
-import { CompanionVariableDefinition } from "@companion-module/base";
+import { CompanionVariableDefinition } from '@companion-module/base'
 
-export function GetVariables(): CompanionVariableDefinition[] {
-    return [
-        { variableId: "version", name: "VSCode version" },
-        { variableId: "language", name: "Language (current file)" },
-        { variableId: "lines", name: "Line count (current file)" },
-        { variableId: "commands", name: "Command count" },
-        { variableId: "status_debug_active_session", name: "Status debug active session" },
-        { variableId: "status_editor_column_number", name: "Status editor column number" },
-        { variableId: "status_editor_document_name", name: "Status editor document name" },
-        { variableId: "status_editor_encoding", name: "Status editor encoding" },
-        { variableId: "status_editor_error_count", name: "Status editor error count" },
-        { variableId: "status_editor_language_id", name: "Status editor LanguageId" },
-        { variableId: "status_editor_line_number", name: "Status editor line number" },
-        { variableId: "status_git_branch", name: "Status git branch" },
-        { variableId: "status_workspace_name", name: "Status workspace name" },
-    ];
-}
+export const stringVariables: CompanionVariableDefinition[] = [
+	{ variableId: 'version', name: 'VSCode version' },
+	{ variableId: 'language', name: 'Language (current file)' },
+	{ variableId: 'status_editor_document_name', name: 'Editor document name' },
+	{ variableId: 'status_editor_encoding', name: 'Editor encoding' },
+	{ variableId: 'status_editor_language_id', name: 'Editor language id' },
+	{ variableId: 'status_git_branch', name: 'Git branch' },
+	{ variableId: 'status_workspace_name', name: 'Workspace name' },
+	{ variableId: 'response_alert', name: 'Last notification response' },
+	{ variableId: 'response_input', name: 'Last input response' },
+	{ variableId: 'response_pick', name: 'Last picker response' },
+]
+
+export const numberVariables: CompanionVariableDefinition[] = [
+	{ variableId: 'lines', name: 'Line count (current file)' },
+	{ variableId: 'tab_size', name: 'Tab size (current file)' },
+	{ variableId: 'commands', name: 'Command count' },
+	{ variableId: 'status_editor_column_number', name: 'Editor current column' },
+	{ variableId: 'status_editor_line_number', name: 'Editor current line' },
+	{ variableId: 'status_editor_error_count', name: 'Editor error count' },
+]
+
+export const booleanVariables: CompanionVariableDefinition[] = [
+	{ variableId: 'status_debug_active_session', name: 'Active debug session' },
+]
+
+export const variables: CompanionVariableDefinition[] = [...stringVariables, ...numberVariables, ...booleanVariables]
